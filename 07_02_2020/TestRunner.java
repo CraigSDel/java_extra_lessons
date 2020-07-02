99c0f27026d5aa3384c4d22d19b96186ac4da35f
craigstroberg <Thiijool1Athav3l>
2020-07-02 19:54:03 +0200

    first commit

---
+++ 07_02_2020/TestRunner.java
@@ -0,0 +1,9 @@
+public class TestRunner{
+
+	public static void main(String[] args){
+		// Initalize a Runner instance 
+		Runner runner = new Runner("Jane", 17);
+		runner.cellphone = "049 451 4563";
+		System.out.println(runner.toString());	
+	}
+}
